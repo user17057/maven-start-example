ad4cb65cb9238b843476f42483b2f11fef2a3b65
stevetheghost <steven.wojtan.15@cnu.edu>
2025-12-29 11:14:52 -0500

    commit

--- src/main/java/com/steven/App.java
+++ src/main/java/com/steven/App.java
@@ -1,5 +1,5 @@
 package com.steven;
-
+import org.mpxj.mpp.ColorType;
 /**
  * Hello world!
  *
@@ -8,6 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	var blah = ColorType.AQUA;
+        System.out.println( "Hello World!" + blah.toString() );
     }
 }

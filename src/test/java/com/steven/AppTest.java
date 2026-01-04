78be2ce978f49a8c9ac90db923c9f04bbaeb064b
stevetheghost <steven.wojtan.15@cnu.edu>
2026-01-04 15:20:18 -0500

    commit

--- src/test/java/com/steven/AppTest.java
+++ src/test/java/com/steven/AppTest.java
@@ -1,37 +1,16 @@
 package com.steven;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
 
 /**
  * Unit test for simple App.
  */
 public class AppTest
-    extends TestCase
+    
 {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
-
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
+    @Test
+    public void test()
     {
         assertTrue( true );
     }
